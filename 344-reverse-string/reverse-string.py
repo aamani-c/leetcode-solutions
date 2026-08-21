f15935a15106e9a161f8ae8749675198df68152d
AAMANI C <varmac382@gmail.com>
2026-08-21 19:42:32 +0530

    Time: 6 ms (12.04%) | Memory: 23.7 MB (5.05%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        def helper(left,right):
-            if left>right:
-                return
+        left=0
+        right=len(s)-1
+        while left<right:
             s[left],s[right]=s[right],s[left]
-            helper(left+1, right-1)
-        helper(0, len(s)-1)
+            left+=1
+            right-=1
